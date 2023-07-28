f3118ca9e7d242db8670fa73ce7b470395080809
Tejass2712 <140151256+Tejass2712@users.noreply.github.com>
2023-07-28 10:43:50 +0530

    navbar

---
+++ src/component/News.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        This is  a news component
+        <NewsItem/>
+      </div>
+    )
+  }
+}
+export default News
---
+++ src/component/NewsItem.jsx
@@ -0,0 +1,15 @@
+ import React, { Component } from 'react'
+ 
+ export class NewsItem extends Component {
+   render() {
+     return (
+       <div>
+         This is news component
+          
+       </div>
+     )
+   }
+ }
+ 
+ export default NewsItem
+ 
